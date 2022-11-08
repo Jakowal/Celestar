ff6f91e783c28aa79f15fc778b2880f03d158122
Jakob Walmann <jakob.walmann@inmeta.no>
2022-11-08 13:48:53 +0100

    Initial commit

---
+++ src/main/kotlin/com/example/celestar/CelestarApplication.kt
@@ -0,0 +1,11 @@
+package com.example.celestar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CelestarApplication
+
+fun main(args: Array<String>) {
+	runApplication<CelestarApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/celestar/CelestarApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.celestar
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CelestarApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
