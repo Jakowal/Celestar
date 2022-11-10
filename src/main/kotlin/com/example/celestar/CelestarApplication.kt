e6cc15946f592b959c30267638e37d46b6d4a99b
jakwalma <jakob.walmann@outlook.com>
2022-11-10 13:18:23 +0100

    Potential fix for Azure tests failing

--- src/main/kotlin/com/example/celestar/CelestarApplication.kt
+++ src/main/kotlin/com/example/celestar/CelestarApplication.kt
@@ -1,9 +1,10 @@
 package com.example.celestar
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [MongoAutoConfiguration::class])
 class CelestarApplication
 
 fun main(args: Array<String>) {
